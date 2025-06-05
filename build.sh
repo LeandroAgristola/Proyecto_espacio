2cc9b713e98bff27bd0133abc8e381e17f0f30a1
LeandroAgristola <leandro.agristola@gmail.com>
2025-06-04 23:10:47 -0300

    cambios en build

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Instala las dependencias. La ruta ya es correcta si build.sh está en la raíz.
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-python create_superuser.py  # Ejecuta el script de superusuario
+
+# Ejecuta collectstatic (nota la ruta a manage.py)
+python espacio/manage.py collectstatic --noinput
+
+# Ejecuta migraciones (nota la ruta a manage.py)
+python espacio/manage.py migrate
+
+# Ejecuta el script de superusuario (nota la ruta al script)
+python espacio/create_superuser.py
