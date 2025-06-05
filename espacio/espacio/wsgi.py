e7cd09f8291320539776e27378d96d7473a2b779
LeandroAgristola <leandro.agristola@gmail.com>
2025-06-05 00:31:38 -0300

    cambios en wsgi

--- espacio/espacio/wsgi.py
+++ espacio/espacio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacio.espacio.settings')
 
 application = get_wsgi_application()
