b8bbf66a3514f19bdcfc9f10fcfbb07fded339a4
LeandroAgristola <leandro.agristola@gmail.com>
2025-06-05 00:14:14 -0300

    CambiosB

--- build.sh
+++ build.sh
@@ -10,17 +10,9 @@ python espacio/manage.py collectstatic --noinput
 # Ejecuta migraciones (la ruta correcta a manage.py)
 python espacio/manage.py migrate
 
-# Establece PYTHONPATH para que Python pueda encontrar tus aplicaciones
+# Establece PYTHONPATH para que Python pueda encontrar tus aplicaciones (ej. webPublic)
 # Añade la carpeta 'espacio/' donde residen tus apps
 export PYTHONPATH=$PYTHONPATH:./espacio/
-
-# --- ¡NUEVAS LÍNEAS DE DIAGNÓSTICO PROFUNDO AQUÍ! ---
-echo "--- Contenido DETALLADO de espacio/ (proyecto Django principal) ---"
-ls -la espacio/
-echo "--- Contenido DETALLADO de espacio/espacio/ (donde está settings.py) ---"
-ls -la espacio/espacio/
-# --- FIN DIAGNÓSTICO PROFUNDO ---
-
 
 # Ejecuta el script de superusuario (la ruta explícita al script)
 python ./create_superuser.py
