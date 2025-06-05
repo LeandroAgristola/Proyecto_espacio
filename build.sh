bc510f50b9dfd1e2fbd972f106954ea0aa5da509
LeandroAgristola <leandro.agristola@gmail.com>
2025-06-05 18:41:17 -0300

    Cambios en build prueba 2

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# ESTO ES CLAVE: Añade la carpeta 'espacio/' (donde residen manage.py y tus apps) al PYTHONPATH.
-# Esto permite que Python encuentre 'webPublic' y otras apps directamente.
-export PYTHONPATH=$PYTHONPATH:./espacio/
+# Correctly adds the project root to PYTHONPATH, allowing Django to find 'espacio.webPublic'
+export PYTHONPATH=$PYTHONPATH:.
 
 # Ejecuta collectstatic
 python espacio/manage.py collectstatic --noinput
